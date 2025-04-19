dc667ebf0097620caecf87ae94aa66f382cf6e48
root <root@charliesurface.>
2025-04-19 17:32:02 -0400

    yayy bug fixes

--- src/Proof.cpp
+++ src/Proof.cpp
@@ -396,7 +396,7 @@ void bcondR(TruthNode* assume, std::vector<Step> *proof){
             third_layer->push_back(Step(
                 goal,
                 InferenceRule::DisjunctionIntroduction,
-                StepLoc{proof, proof->size()-1}
+                StepLoc{second_layer, second_layer->size()-1}
             ));
             third_layer->push_back(Step(
                 Expr(ExprType::Contradiction),
